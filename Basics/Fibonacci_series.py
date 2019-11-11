e5566dfc17187d9d80a23c9d357de1f05b83934f
Gul Pagaria <47470422+gul18@users.noreply.github.com>
2019-11-11 12:15:08 +0530

    Create Fibonacci_series.py

---
+++ Basics/Fibonacci_series.py
@@ -0,0 +1,10 @@
+n = int(input("enter number of digits you want in series: "))
+first = 0
+second = 1
+print("\nfibonacci series is:")
+print(first,",",second, end=",")
+for i in range(2, n):
+    next = first + second
+    print(next, end=",")
+    first = second
+    second = next
